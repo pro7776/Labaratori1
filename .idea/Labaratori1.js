b4bbaa2cf0824c97d72e2ecb3892729542f61b38
Алексей <aleksej.vinogradov.2001@mail.ru>
2025-02-09 16:24:22 +0700

    initial commit

--- .idea/Labaratori1.js
+++ .idea/Labaratori1.js
@@ -93,4 +93,167 @@ console.log(str - a6);
 console.log(str * "2");
 console.log(str / 2);
 
-//6 задание
+//7 задание
+
+let a7 ="12";
+let b7 = "7.15";
+let c7 = Math.round(Number(a7)%Number(b7))
+console.log(c7);
+
+
+//8 задание
+
+let x8 = Number(prompt("введите число: "));
+let c8 =(x8**2-7*x8+10)/(x8**2-8*x8+12)
+alert(c8);
+console.log(c8);
+
+
+//9 задание
+
+let x9 = prompt("введите email: ");
+let cx9 = '@';
+
+if (x9.includes(cx9)) {
+  console.log(x9);
+} else {
+   alert("Не найдет символ @,праверьте правильность email");
+  console.log("Не найдет символ @,праверьте правильность email");
+}
+
+
+
+//2.1
+
+let age = Number(prompt("введите возраст: "));
+
+if (age>=1 && age <=17){
+ alert("Для детей");
+   console.log("Для детей");
+};
+if (age>=18 && age<=30){
+ alert("Для молодежи");
+   console.log("Для молодежи");
+};
+if (age<1 || age>30){
+ alert("Для всех возростов");
+ console.log("Для всех возростов");
+};
+
+
+
+//2.2
+
+let a2_2 = Number(prompt("введите число1: "));
+let b2_2 = Number(prompt("введите число2: "));
+let max = a2_2>b2_2?a2_2:b2_2;
+console.log(max);
+
+
+//2.3
+
+
+
+let a2_3 = Number(prompt("введите число ворон: "));
+if (a>=1 || a<=10)
+{
+switch(a2_3) {
+  case 1:
+  alert("На ветке сидит одна ворона");
+   console.log("На ветке сидит одна ворона");
+    break;
+
+  case 2:
+    alert("На ветке сидит две вороны");
+       console.log("На ветке сидит две вороны");
+    break;
+    case 3:
+        alert("На ветке сидит три вороны");
+           console.log("На ветке сидит три  вороны");
+    break;
+    case 4:
+        alert("На ветке сидит четыре вороны");
+           console.log("На ветке сидит четыре вороны");
+    break;
+    case 5:
+            alert("На ветке сидит пять ворон");
+               console.log("На ветке сидит пять ворон");
+    break;
+    case 6:
+                alert("На ветке сидит шесть ворон");
+                   console.log("На ветке сидит шесть ворон");
+    break;
+    case 7:
+                alert("На ветке сидит семь ворон");
+                   console.log("На ветке сидит семь ворон");
+    break;
+    case 8:
+                alert("На ветке сидит восемь ворон");
+                   console.log("На ветке сидит восемь ворон");
+    break;
+    case 9:
+                alert("На ветке сидит девять ворон");
+                   console.log("На ветке сидит девять ворон");
+    break;
+    case 10:
+                alert("На ветке сидит десять ворон");
+                   console.log("На ветке сидит десять ворон");
+    break;
+
+
+
+}
+}
+else
+{
+alert("Число ворон должно быть от 1 до 10");
+};
+
+
+
+//2.4
+
+let a2_4 = 0;
+
+while (a2_4<=50)
+{ if(a2_4%2!=0)
+{
+console.log(a2_4);
+}
+a2_4++;
+};
+let b2_4;
+
+for (b2_4=0;b2_4<=50;b2_4++)
+{ if(b2_4%2!=0)
+{
+console.log(b2_4);
+}
+};
+
+
+
+//2.5
+
+
+let a2_5;
+let b2_5=0;
+for (a2_5=0;a2_5<=15;a2_5++)
+{ if(a2_5!=5 && a2_5!=7)
+{
+b2_5=a2_5+b2_5;
+}
+};
+console.log(b2_5);
+
+//2.6
+let a2_6 = Number(prompt("введите число x: "));
+let b2_6 = Number(prompt("введите число y: "));
+let c2_6=1;
+let i2_6=0;
+while(i2_6<b2_6)
+{
+c2_6=c2_6*a2_6;
+i2_6++;
+}
+console.log(c2_6);
