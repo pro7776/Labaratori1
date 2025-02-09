cf33fcb06aa1570649d85e25f2d3b08e7c3490b0
Алексей <aleksej.vinogradov.2001@mail.ru>
2025-02-09 15:15:02 +0700

    initial commit

---
+++ .idea/Labaratori1.js
@@ -0,0 +1,96 @@
+//1 задание
+
+
+let a = 43;let b ;
+let c = "dfhrdg";let d = null;
+let f = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof f);
+//2 задание
+
+
+let a2 = 43;let b2 = 56;
+if (a2==b2)
+{
+console.log("true");}
+else
+console.log("false");
+if (a2<b2)
+ {
+ console.log("true"); }
+ else
+ console.log("false");
+if (a2<=b2)
+{console.log("true");
+}else
+console.log("false");
+if (a2>b2)
+{console.log("true");
+}else
+console.log("false");
+//3 задание
+
+
+let a3 = false;
+let b3 = null;
+let c3 ;
+console.log( a3);
+console.log(b3);
+console.log( c3);
+//4 задание
+
+
+let a4 ="1"+2+3;
+console.log( a4);
+let b4 =1+2+"3";
+console.log( b4);
+let c4 ="1"-2;
+console.log( c4);
+let d4 = "1"+-2;
+console.log( d4);
+let f4 = "1"+"1"-"1";
+console.log( f4);
+let e4 ="foo"+-"bar";
+console.log( e4);
+let q4 = 0=="0";
+console.log( q4);
+let w4 = 0.5+0.1==0.6;
+console.log( w4);
+let r4 = 0.1+0.2==0.3;
+console.log( r4);
+let t4 = true+true+true==3;
+console.log( t4);
+let y4 = true==1;
+console.log( y4);
+let z4 = true===1;
+console.log( z4);
+let x4 = 1<2<3;
+console.log( x4);
+let v4 = 3>2>1;
+console.log( v4);
+
+let g4 = 9007199254740991+1==9007199254740991+2;console.log( g4);
+let h4 = Math.sqrt(-1) == Math.sqrt(-1);
+console.log( h4);
+//5 задание
+
+
+let str1="кто ";
+let str2="ты ";let str3="такой?";
+let concatenation= str1 + str2 + str3;
+console.log( concatenation);
+//6 задание
+
+
+
+let str = "20";
+let a6 = 5;
+console.log(str + a6);
+console.log(str - a6);
+console.log(str * "2");
+console.log(str / 2);
+
+//6 задание
